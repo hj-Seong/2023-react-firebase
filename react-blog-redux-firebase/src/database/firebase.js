a10c7a9e622d4178e9437a622d9d08cc7bf357fc
HJ S <hjseong1222@gmail.com>
2023-06-07 12:02:16 +0900

    0607 리액트 블로그 초기화

---
+++ react-blog-redux-firebase/src/database/firebase.js
@@ -0,0 +1,17 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyDKbiUvoi8l0hQmkg_8vXNPMpKw-Is72bI",
+  authDomain: "firestore-7f54d.firebaseapp.com",
+  projectId: "firestore-7f54d",
+  storageBucket: "firestore-7f54d.appspot.com",
+  messagingSenderId: "201560788188",
+  appId: "1:201560788188:web:d7a2f4805f93d62efcb819"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
