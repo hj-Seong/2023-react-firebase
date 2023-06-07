1665cb43f8e80119a1629e8f0b9e24e68a8f3701
HJ S <hjseong1222@gmail.com>
2023-06-07 13:17:56 +0900

    0607 리액트 블로그 로그인 정보 파이어베이스 저장

--- react-blog-redux-firebase/src/database/firebase.js
+++ react-blog-redux-firebase/src/database/firebase.js
@@ -1,5 +1,8 @@
 // Import the functions you need from the SDKs you need
 import { initializeApp } from "firebase/app";
+import { getAuth } from "firebase/auth"
+import { getFirestore}  from 'firebase/firestore'
+
 // TODO: Add SDKs for Firebase products that you want to use
 // https://firebase.google.com/docs/web/setup#available-libraries
 
@@ -15,3 +18,9 @@ const firebaseConfig = {
 
 // Initialize Firebase
 const app = initializeApp(firebaseConfig);
+
+// 인증에 관한 내용 추가
+export const auth = getAuth(app);
+
+// 데이터베이스에 관한 내용 추가
+export const db = getFirestore(app);
--- react-blog-redux-firebase/src/page/Main.jsx
+++ react-blog-redux-firebase/src/page/Main.jsx
@@ -1,7 +1,59 @@
+import { GoogleAuthProvider, signInWithPopup } from 'firebase/auth';
 import React from 'react'
+import { auth, db } from '../database/firebase';
+import { doc, getDoc, setDoc } from 'firebase/firestore';
 
 export default function Main() {
+  
+  // 유저 추가 함수 - firestore 작성
+  // user {uid, email} 
+  const addUser = async(user) => {
+    await setDoc(doc(db, "users", user.uid), user);
+  }
+
+  // 유저 확인 함수 - firestore 작성
+  const checkUser = async(user) => {
+    const docRef = await getDoc(doc(db, "users", user.uid))
+    // exists() 함수는 getDoc을 통해 가져온 값이 있으면 true
+    // 없으면 false
+    if(!docRef.exists()) {
+        addUser(user);
+    }else {
+        console.log("가입되어 있습니다")
+    }
+  }
+
+  // 구글 로그인 함수
+  const onGoogleLogin = () => {
+    const provider = new GoogleAuthProvider();
+
+    signInWithPopup(auth, provider)
+    .then((result) => {
+        const credential = GoogleAuthProvider.credentialFromResult(result);
+        const token = credential.accessToken;
+        // 구글로 로그인 후 정보값 가져옴
+        const user = result.user;
+        console.dir(user);
+        
+        // 로그인했다면, uid를 확인후 firestore에 저장
+        checkUser({uid : user.uid, email : user.email});
+
+    }).catch((error) => {
+        // Handle Errors here.
+        const errorCode = error.code;
+        const errorMessage = error.message;
+        // The email of the user's account used.
+        const email = error.customData.email;
+        // The AuthCredential type that was used.
+        const credential = GoogleAuthProvider.credentialFromError(error);
+        // ...
+    });
+  }
   return (
-    <div>Main</div>
+    <div>
+        <h3>Main</h3>
+        <button onClick={onGoogleLogin}>구글로 로그인</button>
+        <h2>{}님 환영합니다</h2>
+    </div>
   )
 }
